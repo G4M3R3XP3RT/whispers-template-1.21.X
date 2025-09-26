ccaebd3813e2d47c86a343108e3e71e0c9976971
G4M3R3XP3RT <jeremiemarchandwd@outlook.com>
2025-09-26 15:09:59 -0400

    Initial commit

---
+++ src/main/java/net/g4m3r/whispers/Whispers.java
@@ -0,0 +1,16 @@
+package net.g4m3r.whispers;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Whispers implements ModInitializer {
+	public static final String MOD_ID = "whispers";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/g4m3r/whispers/WhispersClient.java
@@ -0,0 +1,11 @@
+package net.g4m3r.whispers;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WhispersClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
